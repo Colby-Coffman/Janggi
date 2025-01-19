751d847e188460e6d89cf0949464aec81ed37d30
Colby-Coffman <colbycoffman20@gmail.com>
2025-01-19 11:53:10 -0600

    Box

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
-
 function main() {
-    console.log("Started");
+    console.log("\u250C--\u2510");
+    console.log("|\\ |");
+    console.log("| \\|");
+    console.log("\u2514--\u2518");
 }
-
-
 
 if (require.main === module) {
     main();
