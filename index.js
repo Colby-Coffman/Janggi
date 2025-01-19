abce475eafe07e5388c466e0ffb43df7284cf7d3
Colby-Coffman <colbycoffman20@gmail.com>
2025-01-19 17:23:31 -0600

    Both boards for reference

--- index.js
+++ index.js
@@ -130,6 +130,68 @@ function main() {
 	    console.log();
 	}
     }
+    for (let row = 0; row < 8; row++) {
+	for (let column = 0; column < 8; column++) {
+	    strToPrint = "\u2500";
+	    if (row === 0) {
+		if (column === 0) {
+		    strToPrint = "\u250C" + strToPrint;
+		}
+		strToPrint += "\u252C";
+		if (column === 7) {
+		    strToPrint = strToPrint.slice(0, -1);
+		    strToPrint += "\u2510";
+		}
+		process.stdout.write(strToPrint);
+	    }
+	    else {
+		if (column === 0) {
+		    strToPrint = "\u251C" + strToPrint;
+		}
+		strToPrint += "\u253C";
+		if (column === 7) {
+		    strToPrint = strToPrint.slice(0, -1);
+		    strToPrint += "\u2524";
+		}
+		process.stdout.write(strToPrint);
+	    }
+	}
+	console.log();
+	for (let column = 0; column < 8; column++) {
+	    strToPrint = "\u2502";
+	    if (column === 7) {
+		strToPrint += " \u2502";
+	    }
+	    else if ((([0, 6].includes(row)) && (column === 3)) ||
+		     (([1, 7].includes(row)) && (column === 4))) {
+		strToPrint += "\\"
+	    }
+	    else if ((([0, 6].includes(row)) && (column === 4)) ||
+		     ([1, 7].includes(row)) && (column === 3)) {
+		strToPrint += "/"
+	    }
+	    else {
+		strToPrint += " ";
+	    }
+	    process.stdout.write(strToPrint);
+	}
+	console.log();
+	if (row === 7) {
+	    for (let column = 0; column < 8; column++) {
+		strToPrint = "\u2500";
+		if (column === 0) {
+		    strToPrint = "\u2514" + strToPrint;
+		}
+		strToPrint += "\u2534"
+		if (column === 7) {
+		    strToPrint = strToPrint.slice(0, -1);
+		    strToPrint += "\u2518";
+		}
+		process.stdout.write(strToPrint);
+	    }
+	    console.log();
+	}
+    }
 }
 
 if (require.main === module) {
