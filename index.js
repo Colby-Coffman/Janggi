07a6825da0d969abc9bdcd8e2a45a1374457d045
Colby-Coffman <colbycoffman20@gmail.com>
2025-01-19 12:22:29 -0600

    One line so wow

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 function main() {
+    for (let i = 0; i < 8; i++) {
+	strToPrint = "\u2500\u2500"
+	if (i == 0) {
+	    strToPrint = "\u250C" + strToPrint
+	}
+	strToPrint += "\u252C"
+	if (i == 7) {
+	    strToPrint = strToPrint.slice(0, -1)
+	    strToPrint += "\u2510"
+	}
+	process.stdout.write(strToPrint);
+    }
+}
+
+function printCellWithDiagonal() {
     console.log("\u250C--\u2510");
     console.log("|\\ |");
     console.log("| \\|");
+    console.log("\u2514--\u2518");
+}
+
+function printEmptyCell() {
+    console.log("\u250C--\u2510");
+    console.log("|  |");
+    console.log("|  |");
     console.log("\u2514--\u2518");
 }
 
